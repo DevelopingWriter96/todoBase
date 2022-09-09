eaef805464fd933eab9c65cbc61e92958cfe99ed
DevelopingWriter96 <staceyrichard1996@gmail.com>
2022-09-09 16:29:18 -0600

    first base commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-import "./styles.css";
+const todoList = document.querySelector('.todoList');
+
+let todos = [
+    {
+        Name:"Sample1",
+        Status:"Complete"
+    },
+    {
+        Name:"Sample2",
+        Status:"Incomplete"
+    }
+]
+
+function todoItem(Name, Status) {
+    let listItem = document.createElement('li');
+    listItem.className = Status
+}
+
+function todoList(todos) {
+    while(let i = 0; i <= todos.length; i++;) {
+        todoItem(todos[i].name, todo[i].status);
+    }   
+}
+
+todoList(todos);
