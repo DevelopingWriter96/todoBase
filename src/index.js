f1b283bfaef7c8ed10f0f7d2131b45fe76d61e57
DevelopingWriter96 <staceyrichard1996@gmail.com>
2022-09-09 20:38:03 -0600

    Two functions working

--- src/index.js
+++ src/index.js
@@ -7,19 +7,53 @@ let todos = [
     },
     {
         Name:"Sample2",
-        Status:"not done"
+        Status:""
     }
 ]
 
 function todoItems(list) {
-    let i = 0;
-    while (i > list.length, i++) {
+    // let todoList = document.getElementsByClassName("todoList");
+    console.log(list);
+    list.forEach(function(todo) {
         let listItem = document.createElement('li');
-        listItem.textContent = list[i].Name;
-        listItem.className = list[i].Status;
+        let listTrashSpan = document.createElement('span');
+        let listTrashIcon = document.createElement('i');
+        listItem.textContent = todo.Name
+        listItem.className = todo.Status;
+        listTrashIcon.className = "fa fa-trash"
+        listTrashSpan.appendChild(listTrashIcon);
+        if (todo.Status == "") {
+            let listEditSpan = document.createElement('span');
+            listEditSpan.className = "editBtn"
+            let listEditIcon = document.createElement('i');
+            listEditIcon.className = "fa fa-edit"
+            listEditIcon.textContent = ""
+            listEditSpan.appendChild(listEditIcon);
+            listItem.appendChild(listEditSpan);
+        }
+        listItem.appendChild(listTrashSpan);
         todoList.appendChild(listItem);
-        console.log(listItem)
-    }
+    })      
+}
+
+function completeNumber(list) {
+    const doneNum = document.querySelector('.doneNum')
+    let i = 0;
+    list.forEach(function(todo) {
+        if (todo.Status == "") {
+            i++
+        }
+    });
+    doneNum.textContent = "You have " + i + " pending tasks."
+}
+
+function removeComplete(list) {
+    let completeItems = list.filter(todo => todo.Status == "done");
+    console.log(completeItems);
 }
 
 todoItems(todos);
+
+completeNumber(todos);
+
+removeComplete(todos);
